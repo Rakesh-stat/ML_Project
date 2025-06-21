ff43e5f41b9799d5ebd4e4200feed2227b08aae3
Rakesh-stat <rdathangari@gmail.comgit config --global user.name Rakesh-stat>
2025-06-21 00:26:56 -0700

    s

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="ml_project",
+    version="0.1.0",
+    description="End to End ML project",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[],
+)
